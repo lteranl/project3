c9cc1fce3ce09d43efdcc3bd6ad857c7e6b1d0c2
Cole Jackson <cole_jackson@live.com>
2020-07-30 19:53:17 -0700

    added Cart component

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Client from "shopify-buy"
 
+const client = Client.buildClient({
+  storefrontAccessToken: "123f6f392a6b7208a7cb7cb9caf217f1",
+  domain: "project3uci.myshopify.com"
+})
 
 ReactDOM.render(
 
