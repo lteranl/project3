137bd8b081cc80958475a25cf417319df167ef1e
cole jackson <cole_jackson@live.com>
2020-08-06 15:42:08 -0700

    removed shopify components too difficult to work with will rebuild cart once things are going. working on products route, added dummy data as a json file so i can upload to the db so it doesnt need to be in the repo

--- routes/api/products.js
+++ routes/api/products.js
@@ -5,39 +5,85 @@ const { db } = require("../../models/Products");
 const { default: Product } = require("../../client/src/components/shopify/Product");
 const app = express()
 
-router.get("/products_by_id", (req, res) => {
-    Product.find({ })
-    .then((data) => {
-        console.log("data:", data);
-        res.json(data);
+router.post("/Products", (req, res) => {
 
-    })
-    .catch((error) => {
-        console.log('error:', error)
-    })
-})
+    let order = req.body.order ? req.body.order : "desc";
+    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
+    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
+    let skip = parseInt(req.body.skip);
 
-router.post("/dashboard", (req, res) => {
-    const data = req.body;
-    const addProduct = new Product(data);
+    let findArguments = {};
+    let term = req.body.searchTerm;
 
-    addProduct.save((error) => {
-        if (error) {
-            res.status(500).json({ msg: "Error"})
-            return;
+    for (let key in req.body.filters) {
+
+        if (req.body.filters[key].length > 0) {
+            if (key === "price") {
+                findArguments[key] = {
+                    $gte: req.body.filters[key][0],
+                    $lte: req.body.filters[key][1]
+                }
+            } else {
+                findArguments[key] = req.body.filters[key];
+            }
         }
-        return res.json({
-            msg: "added to cart"
-        })
-    })
-})
-
-router.get('/name', (req, res) => {
-    const data = {
-        name: "blouse",
-        description: "Blue Blouse 1970s",
-        price: "$100"
     }
-})
+
+    console.log(findArguments)
+
+    if (term) {
+        Product.find(findArguments)
+            .find({ $text: { $search: term } })
+            .populate("writer")
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({ success: false, err })
+                res.status(200).json({ success: true, products, postSize: products.length })
+            })
+    } else {
+        Product.find(findArguments)
+            .populate("writer")
+            .sort([[sortBy, order]])
+            .skip(skip)
+            .limit(limit)
+            .exec((err, products) => {
+                if (err) return res.status(400).json({ success: false, err })
+                res.status(200).json({ success: true, products, postSize: products.length })
+            })
+    }
+
+});
+
+
+
+router.get("/products_by_id", (req, res) => {
+    let type = req.query.type
+    let productIds = req.query.id
+
+    console.log("req.query.id", req.query.id)
+
+    if (type === "array") {
+        let ids = req.query.id.split(',');
+        productIds = [];
+        productIds = ids.map(item => {
+            return item
+        })
+    }
+
+    console.log("productIds", productIds)
+
+
+    Product.find({ '_id': { $in: productIds } })
+        .populate('writer')
+        .exec((err, product) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).send(product)
+        })
+});
+
+
 
 module.exports = router;
+router.post("/")
