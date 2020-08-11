a71bc1380bb8b7bcc2ae194cf21f6e74994bbb61
cole jackson <cole_jackson@live.com>
2020-08-11 14:22:37 -0700

    got the page back

--- client/src/components/layout/Pay.js
+++ client/src/components/layout/Pay.js
@@ -1,4 +1,70 @@
-import React from 'react';
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import { Button, AppBar, Toolbar, Typography } from "@material-ui/core/";
+
+class Payment extends Component {
+    constructor() {
+      super();
+      this.state = {
+        email: "",
+        password: "",
+        errors: {},
+      };
+    }
+
+render() {
+    const { errors } = this.state;
+    return(
+        <div className="container">
+            <AppBar id="appbar" position="sticky" color="secondary">
+                <Toolbar>
+                    <Typography className="grannies" variant="h4">
+                        Grannies
+                    </Typography>
+                    <Button href="/" color="inherit">
+              Home
+            </Button>
+            <Button href="/Register" color="inherit">
+              Register
+            </Button>
+                </Toolbar>
+            </AppBar>
+            <div style={{ marginTop: "3rem" }} className="row">
+                <div className="col s8 offset-s2">
+                    <div className="col s12" style={{ paddingLeft: '11px'}}>
+                        <h3> Payment Info </h3>
+                        <br/>
+                        <form>
+                                <input type="text"/>
+                                <label htmlFor="fullName">Full Name: </label>
+                                <input type="text"/>
+                                <label htmlFor="cardNumber">Card Number: </label>
+                                <input type="text"/>
+                                <label htmlFor="exp">Exp Date: </label>
+                                <input type="text"/>
+                                <label htmlFor="securityCode">Security Code:</label>
+                                <input type="text"/>
+                                <label htmlFor="addOne">Address line 1: </label>
+                                <input type="text"/>
+                                <label htmlFor="addTwo">Address line 2: </label>
+                                <br/>
+                                <br/>
+                                <br/>
+                                <input id="payBtn" variant="pay now" className="btn btn-flat waves-effect" type="submit"/>
+                                
 
 
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}}
+
+export default Payment;
